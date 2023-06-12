2339470294dda358e598a2c7101dae794c18f6ce
Sameer Khawaja <sameerkhawaja01@gmail.com>
2023-06-12 16:31:30 -0400

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,23 @@ var createTaskHandler = function (event)
 {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskInputType = document.querySelector("select[name='task-type']").value
+
+    // create list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "my task";
     listItemEl.className = "task-item"
+
+    //create div to hold task info and add list item
+    var taskInfoEl = document.createElement("div")
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskInputType + " </span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add item to list
     tasksToDoEl.appendChild(listItemEl);
+
 }
 
 formEl.addEventListener("submit", createTaskHandler)
