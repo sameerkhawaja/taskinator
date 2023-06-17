a6eeef6d994dbf17a86119f66a2db56af67095de
sameerkhawaja <sameerkhawaja01@gmail.com>
2023-06-17 19:17:41 -0400

    add behavior to move tasks based on status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var taskIdCounter = 0
 var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+
 var pageContentEl = document.querySelector("#page-content")
 
 var taskFormHandler = function (event)
@@ -120,6 +123,23 @@ var taskButtonHandler = function (event)
     }
 }
 
+var taskStatusChangeHandler = function (event){
+    var taskId = event.target.getAttribute("data-task-id")
+    var statusValue = event.target.value.toLowerCase();
+    var taskSelected = document.querySelector(`.task-item[data-task-id='${taskId}']`);
+
+    if (statusValue === "to do"){
+        tasksToDoEl.appendChild(taskSelected)
+    }
+    else if (statusValue === "in progress"){
+        tasksInProgressEl.appendChild(taskSelected)
+    }
+    else if (statusValue === "completed"){
+        tasksCompletedEl.appendChild(taskSelected)
+    }
+
+}
+
 var deleteTask = function (taskId)
 {
     var taskSelected = document.querySelector(`.task-item[data-task-id='${taskId}'`)
@@ -153,3 +173,4 @@ var completeEditTask = function (taskNameInput, taskInputType, taskId)
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
