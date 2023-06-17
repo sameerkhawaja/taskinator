88c2f9986c0622027771a5ee6d9641468d504a09
Sameer Khawaja <sameerkhawaja01@gmail.com>
2023-06-16 22:34:06 -0400

    add delete task functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0
 var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content")
 
 var taskFormHandler = function (event)
 {
@@ -64,7 +65,7 @@ var createTaskActions = function (taskId)
     actionContainerEl.appendChild(editButtonEl);
 
     var deleteButtonEl = document.createElement("button")
-    deleteButtonEl.className = "btn edit-btn"
+    deleteButtonEl.className = "btn delete-btn"
     deleteButtonEl.textContent = "Delete"
     deleteButtonEl.setAttribute("data-task-id", taskId)
 
@@ -92,4 +93,20 @@ var createTaskActions = function (taskId)
 
 }
 
-formEl.addEventListener("submit", taskFormHandler)
+var taskButtonHandler = function (event)
+{
+    if (event.target.matches(".delete-btn"))
+    {
+        var taskId = event.target.getAttribute("data-task-id")
+        deleteTask(taskId);
+    }
+}
+
+var deleteTask = function (taskId)
+{
+    var taskSelected = document.querySelector(`.task-item[data-task-id='${taskId}'`)
+    taskSelected.remove();
+}
+
+formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
